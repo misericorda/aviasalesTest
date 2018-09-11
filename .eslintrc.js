d654e192b94c02339607ce9a6cd9b6ab54c50124
Dmitry Oleynik <johnkeats2011@gmail.com>
2018-09-11 14:20:54 +0300

    Added linter config

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "parser": "babel-eslint",
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "no-console": 0,
+    "no-undef": [0, 0],
+    "react/prop-types": [
+      0,
+      0
+    ],
+    "indent": [
+      0,
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "windows"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
